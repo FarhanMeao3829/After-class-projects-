8e67e6a24f34bac39af7e50ca862aa71aea2327b
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-09-26 09:13:58 +0600

    mao mao

---
+++ Set Symetric Difference.py
@@ -0,0 +1,12 @@
+set1 = {'blue', 'red'}
+set2 = {'blue', 'orange'}
+
+newSet = set1.symmetric_difference(set2)
+print(newSet)
+
+
+set1 = {1,2,3,4,5}
+set2 = {1,5,6,7,8,9,}
+
+newSet = set1.symmetric_difference(set2)
+print(newSet)
