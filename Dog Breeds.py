8fd405912347c985d26e5c8942f2d58ef87faa68
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-10-03 09:10:11 +0600

    w

---
+++ Dog Breeds.py
@@ -0,0 +1,22 @@
+class Dog:
+    animal = "dog"  # Nahian sir is bad mao
+    def __init__(self, name, breed, size, temperament, food):
+        self.name = name
+        self.breed = breed
+        self.size = size
+        self.temperament = temperament
+        self.food = food
+
+# Farhan is good mao
+dog1 = Dog("Rex", "German Shepherd", "Large", "Loyal", "Dry Kibble")
+dog2 = Dog("Buddy", "Labrador Retriever", "Large", "Friendly", "Wet Food")
+dog3 = Dog("Bella", "Chihuahua", "Small", "Alert", "Soft Food")
+
+# Aditya is Suiiii
+for dog in (dog1, dog2, dog3):
+    print(f"animal: {dog.animal}")
+    print(f"name: {dog.name}")
+    print(f"breed: {dog.breed}")
+    print(f"size: {dog.size}")
+    print(f"temperament: {dog.temperament}")
+    print(f"food: {dog.food}\n")
